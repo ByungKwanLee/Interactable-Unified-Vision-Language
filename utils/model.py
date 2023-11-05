4bd322dd96285a1300564f84b941819c35260e2e
ByungKwanLee <leebk@kaist.ac.kr>
2023-11-06 01:20:03 +0900

    v0.1

--- utils/model.py
+++ utils/model.py
@@ -36,7 +36,7 @@ def align_and_update_state_dicts(model_state_dict, ckpt_state_dict):
     unmatched_log = []
     unloaded_log = []
 
-    filtered_ckpt_keys = list(filter(lambda x: not x.startswith('backbone'), ckpt_keys))
+    filtered_ckpt_keys = list(filter(lambda x: not x.startswith('backbone.'), ckpt_keys))
 
     for model_key in model_keys:
         model_weight = model_state_dict[model_key]
